a461ba7a67a6c37fe57e046fdcb976f4de2e067a
anass-dev-ops <anasszakaria2012@gmail.com>
2021-07-09 23:49:40 +0100

    Add Methods in Book Controller

---
+++ src/main/java/org/anassdev/book/crud/service/BookService.java
@@ -0,0 +1,14 @@
+package org.anassdev.book.crud.service;
+
+import java.util.List;
+
+import org.anassdev.book.crud.entity.Book;
+
+public interface BookService {
+	
+	List<Book> getBooks();
+	Book getBookById(Long id);
+	Book addBook(Book book);
+	Book updateBook(Book book);
+	Book deleteBookById(Long id);
+}
---
+++ src/main/java/org/anassdev/book/crud/service/BookServiceImpl.java
@@ -0,0 +1,59 @@
+package org.anassdev.book.crud.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.anassdev.book.crud.entity.Book;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService{
+	
+	private ArrayList<Book> books;
+	private long ct;
+
+	@Override
+	public List<Book> getBooks() {
+		books = new ArrayList<Book>();
+		
+		ct = 1L;
+		Stream.of("JAVA OCA", "Programmation", "JavaScript", 
+					"Angular", "TypeScript")
+			.forEach(title -> {
+			Book book = new Book();
+			book.setId(ct);
+			book.setTitle(title);
+			book.setPrice("2"+(ct*234)+"dhs");
+			book.setDescription("description: " + title);
+			book.setAuthor("author: " + title);
+			book.setEdition("edition: 2.0" );
+			ct += 1L;
+			books.add(book);
+		});
+		
+		return books;
+	}
+
+	@Override
+	public Book getBookById(Long id) {
+		return books.get(id.intValue());
+	}
+
+	@Override
+	public Book addBook(Book book) {
+		books.add(book);
+		return book;
+	}
+
+	@Override
+	public Book updateBook(Book book) {
+		return null;
+	}
+
+	@Override
+	public Book deleteBookById(Long id) {
+		return books.remove(id.intValue());
+	}
+
+}
